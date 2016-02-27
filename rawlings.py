8db7a1ffda141c8dd226448d11d3f592594154c9
Saravanan SJ <sjs7007@gmail.com>
2016-02-27 13:43:18 -0500

    rwalings done

---
+++ rawlings.py
@@ -0,0 +1,6 @@
+import urllib2
+pageHtml = urllib2.urlopen('http://ufl.transloc.com/t/stops/4093246').read()
+temp = '<td class="wait_time time_1" width="110px"><a href="/t/routes/4001170" title="View all of Butler Plaza">'
+startPos = pageHtml.find(temp)+len(temp)
+endPos = pageHtml.find('<',startPos)
+print('12 Rawlings Hall : ',pageHtml[startPos:endPos])
